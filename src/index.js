aa0a2595acacec936c3cb06f1a6213a044427388
Pramesh Karki <Parameswarkarki@gmail.com>
2021-10-26 16:20:18 +0545

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import reactDom from "react-dom";
+
+// *Import components
+import App from "./app/App";
+
+// *Import Global styles
+import "./index.css";
+
+reactDom.render(<App/>,document.getElementById("root"));
